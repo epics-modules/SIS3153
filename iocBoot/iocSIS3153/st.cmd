c36cdf574673e3a98d6dce38938392ee13f1376d
Mark Rivers <rivers@cars.uchicago.edu>
2024-10-06 08:53:40 -0500

    New file

---
+++ iocBoot/iocSIS3153/st.cmd
@@ -0,0 +1,20 @@
+#!../../bin/linux-x86_64/SIS3153App
+
+< envPaths
+
+epicsEnvSet("PREFIX", "SIS1:")
+epicsEnvSet("PORT", "SIS3153")
+
+## Register all support components
+dbLoadDatabase "../../dbd/SIS3153App.dbd"
+SIS3153App_registerRecordDeviceDriver pdbbase
+
+drvSIS3153Configure("$(PORT)")
+
+dbLoadRecords("$(SIS3153)/db/Zygo1404.template", "P=$(PREFIX),PORT=$(PORT)")
+
+asynSetTraceIOMask($(PORT),0,ESCAPE)
+asynSetTraceMask($(PORT),0,ERROR|DRIVER)
+
+iocInit
+
